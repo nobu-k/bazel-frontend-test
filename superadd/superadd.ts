b5e97de97b04d18ff4cb1660f5ab53c2dcc7a821
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2021-04-13 08:20:45 -0700

    Add an example of a library in a subdirectory

--- superadd/superadd.ts
+++ superadd/superadd.ts
@@ -1,3 +1,4 @@
 import { add } from 'myspace/add';
+import { hello } from 'myspace/lib/hello';
 
-export const superadd = (a: number, b: number) => add(a, b);
+export const superadd = (a: number, b: number) => [add(a, b), hello()];
